2fd0526830ddb452dd7a8b6643f29a8503daf5d1
caiquemiranda <caiquemirandas@gmail.com>
2023-06-25 18:08:42 -0300

    img_code

--- app.py
+++ app.py
@@ -2,23 +2,36 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Dados de exemplo para os posts do blog
-posts = [
-    {
-        'titulo': 'Meu primeiro post',
-        'autor': 'João',
-        'conteudo': 'Este é o meu primeiro post no blog!'
-    },
-    {
-        'titulo': 'Segundo post',
-        'autor': 'Maria',
-        'conteudo': 'Aqui está o meu segundo post. Espero que gostem!'
-    }
-]
-
 @app.route('/')
-def home():
-    return render_template('index.html', posts=posts)
+def index():
+    articles = [
+        {
+            'title': 'Título do Artigo 1',
+            'content': 'Conteúdo do Artigo 1...'
+        },
+        {
+            'title': 'Título do Artigo 2',
+            'content': 'Conteúdo do Artigo 2...'
+        },
+        {
+            'title': 'Título do Artigo 3',
+            'content': 'Conteúdo do Artigo 3...'
+        },
+        {
+            'title': 'Título do Artigo 4',
+            'content': 'Conteúdo do Artigo 4...'
+        },
+        {
+            'title': 'Título do Artigo 5',
+            'content': 'Conteúdo do Artigo 5...'
+        },
+        {
+            'title': 'Título do Artigo 6',
+            'content': 'Conteúdo do Artigo 6...'
+        }
+    ]
+
+    return render_template('index.html', articles=articles)
 
 if __name__ == '__main__':
     app.run(debug=True)
